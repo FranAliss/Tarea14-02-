920c1b2ec17861ed3e1453e4b4b72a606c3a1dfd
Fran Aliss <franalissar@gmail.com>
2022-02-14 22:30:34 -0400

    Version con mult

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,14 @@
 import sumar from "./sumador";
+import mult from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const first1 = document.querySelector("#primer-num");
+const second1 = document.querySelector("#segundo-num");
+const form1 = document.querySelector("#mult-form");
+const div1 = document.querySelector("#resultado-prod-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +17,14 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+
+});
+
+form1.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber1 = Number.parseInt(first1.value);
+  const secondNumber1 = Number.parseInt(second1.value);
+
+  div1.innerHTML = "<p>" + mult(firstNumber1, secondNumber1) + "</p>";
 });
